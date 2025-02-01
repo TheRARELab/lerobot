91330d81556126c09920012d5e5b9d4fd939c54e
Amanuel Tadesse Ergogo [aergogo] <aergogo@forest.usf.edu@gaivi-login1.cse.usf.edu>
2025-01-31 21:25:39 -0500

    Add training job submission scripts

---
+++ gaivi/train_aloha_job.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+#All options below are recommended
+#SBATCH -p general # run on partition general
+#SBATCH --cpus-per-task=32 # 32 CPUs per task
+#SBATCH --mem=100GB # 100GB per task
+#SBATCH --gpus=8 # 4 GPUs
+#SBATCH -o std_out
+#SBATCH -e std_err
+#SBATCH --mail-user=aergogo@usf.edu # email for notifications
+#SBATCH --mail-type=BEGIN,END,FAIL,REQUEUE # events for notifications
+export PYTHONPATH=$PYTHONPATH:/home/a/aergogo/lerobot/
+export HYDRA_FULL_ERROR=1
+singularity exec --nv gaivi/lerobot-gpu-hf.sif python lerobot/scripts/train.py \
+    policy=act \
+    env=aloha \
+    env.task=AlohaInsertion-v0 \
+    dataset_repo_id=lerobot/aloha_sim_insertion_human \
